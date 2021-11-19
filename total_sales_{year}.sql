b131c6cb294ab2ee7036bdab8571a19aabb72ae8
Blake Rhoden <zbrhoden@gmail.com>
2021-11-19 15:53:05 -0600

    fixed #9

--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -6,8 +6,3 @@ WHERE InvoiceDate BETWEEN "2009-0-0" and "2009-12-31";
 SELECT SUM(Total)
 FROM Invoice
 Where InvoiceDate BETWEEN "2011-0-0" AND '2011-12-31';
-
-SELECT SUM(Total)
-FROM Invoice
-WHERE InvoiceDate BETWEEN "2009-0-0" and "2009-12-31"
-    or InvoiceDate BETWEEN "2011-0-0" AND '2011-12-31';
